a4aa5532a164cb855661ae284cd9ba648d2c69e2
pavan kumar dasari <pavank.d@gmail.com>
2017-05-17 23:59:01 +0530

    Init Commit

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -17,7 +17,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
 
   db.collection('Todos').find().count().then((count)=>{
     console.log(`Todos count:${count}`);
-    // console.log(JSON.stringify(docs,undefined,2));
   },(err)=>{
     console.log('Unable to fetch todos',err);
   })
