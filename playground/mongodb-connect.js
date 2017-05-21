845529c87af9a65e97380c0cc01be538b9347278
pavan kumar dasari <pavank.d@gmail.com>
2017-05-21 07:15:55 +0530

    update code added

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,25 +7,36 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   }
   console.log('Connected to MongoDB Server');
 
-  // db.collection('Todos').insertOne({
-  //   text:'Something to do',
-  //   completed:false
-  // },(err,result)=>{
-  //   if(err){
-  //     return console.log("Unable to insert ToDo",err);
-  //   }
-  //   console.log(JSON.stringify(result.ops,undefined,2));
-  // })
-  db.collection('Users').insertOne({
-    name:'vamsi Dasari',
-    age:'34',
-    location : 'Munich'
-  },(err,results)=>{
+  db.collection('Todos').insertOne({
+    text:'break fast',
+    completed:true
+  },(err,result)=>{
     if(err){
-      return console.log('Unable to insert User',err);
+      return console.log("Unable to insert ToDo",err);
     }
-    console.log(JSON.stringify(results.ops,undefined,2));
+    console.log(JSON.stringify(result.ops,undefined,2));
   })
+  // db.collection('Users').insertOne({
+  //   name:'vamsi Dasari',
+  //   age:'34',
+  //   location : 'Munich'
+  // },(err,results)=>{
+  //   if(err){
+  //     return console.log('Unable to insert User',err);
+  //   }
+  //   console.log(JSON.stringify(results.ops,undefined,2));
+  // })
+  // db.collection('Users').insertOne({
+  //   name:'Pavan Dasari',
+  //   age:'32',
+  //   location : 'Hyderabad'
+  // },(err,results)=>{
+  //   if(err){
+  //     return console.log('Unable to insert User',err);
+  //   }
+  //   console.log(JSON.stringify(results.ops,undefined,2));
+  // })
+
 
   db.close();
 });
