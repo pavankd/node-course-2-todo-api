052d81490b5fc0680cbb0ea19608350cc709e350
pavankd <pavank.d@gmail.com>
2017-05-24 15:16:08 +0530

    code updated up to secation 8 lecture 90 from office system

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,31 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
 var User = mongoose.model('User',{
   email:{
     type:String,
     required:true,
     minlength:1,
-    trim:true
-  }
+    trim:true,
+    unique:true,
+    validate:{
+      validator:validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:6
+  },
+  tokens:[{
+    access:{
+      type:String,
+      required:true
+    },
+    token:{
+      type:String,
+      required:true
+    }
+  }]
 })
 module.exports={User}
