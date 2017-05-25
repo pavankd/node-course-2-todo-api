92889503a2c7d591ffa5b2c458540f5f618f2b63
pavan kumar dasari <pavank.d@gmail.com>
2017-05-25 13:28:04 +0530

    Add DELETE /user/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,16 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+
+  return user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
+}
+
 UserSchema.statics.findByToken = function(token){
   var User = this;
   var decoded;
@@ -103,6 +113,8 @@ UserSchema.pre('save',function(next){
     next()
   }
 });
+
+
 
 var User = mongoose.model('User',UserSchema)
 module.exports={User}
